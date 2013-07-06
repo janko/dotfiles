b3817e3dca95ac6f02ddb00d90f13a00d774d280
Janko Marohnić <janko.marohnic@gmail.com>
2013-07-06 14:45:18 +0200

    Extract getting ruby version into a function

--- bin/prompt.sh
+++ bin/prompt.sh
@@ -4,13 +4,17 @@ parse_git_branch() {
   echo `/usr/bin/git rev-parse --abbrev-ref HEAD`
 }
 
+get_ruby_version() {
+  echo `rbenv version-name`
+}
+
 set_prompt() {
   local user="$GREEN\u$RESET"
   local path="\w"
   if [ -d .git ]; then
     local git_branch="$RED`parse_git_branch`$RESET, "
   fi
-  local ruby_version="$YELLOW`rbenv version-name`$RESET"
+  local ruby_version="$YELLOW`get_ruby_version`$RESET"
 
   PS1="$user:$path[$git_branch$ruby_version]$ "
 }
