7b85d87f2dd69c9f13e6f44e9078842a63c6b993
Janko Marohnić <janko.marohnic@gmail.com>
2014-08-23 09:34:25 +0200

    Add Cucumber to Spring

--- .spring.rb
+++ .spring.rb
@@ -15,4 +15,19 @@ module Spring
     Spring.register_command "rspec", RSpec.new
     Spring::Commands::Rake.environment_matchers[/^spec($|:)/] = "test"
   end
+
+  module Commands
+    class Cucumber
+      def env(*)
+        "test"
+      end
+
+      def exec_name
+        "cucumber"
+      end
+    end
+
+    Spring.register_command "cucumber", Cucumber.new
+    Spring::Commands::Rake.environment_matchers[/^cucumber($|:)/] = "test"
+  end
 end
